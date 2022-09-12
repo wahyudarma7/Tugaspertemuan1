8964d23eddf85509e00f04b2a89a8212a47ef290
Wahyudarma7 <darmawahyu836@gmail.com>
2022-09-12 22:16:05 +0700

    penambahan isi Contoh1.php

--- Contoh1.php
+++ Contoh1.php
@@ -1,8 +1,10 @@
 <?php
-class Contoh1 extends CI_Controller
+class CONTOH1 extends CI_Controller
 {
     public function index()
-    {echo "<h1>Perkenalkan</h1>";
-        echo"Nama saya Wahyu Darma Nuraprilian Saya tinggal di daerah tomang yang saya sukai adalah sepakbola";
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo"Nama saya Wahyu Darma Nuraprilian Saya tinggal di daerah tomang olah raga yang saya sukai adalah sepakbola";
+
     }
 }
