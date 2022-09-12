2af2b015ba906cf3add1c74913debd5dc89204ad
Wahyudarma7 <darmawahyu836@gmail.com>
2022-09-12 20:08:04 +0700

    menambahkan Contoh1.php,Welcome.php,index.html

---
+++ Contoh1.php
@@ -0,0 +1,8 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index()
+    {echo "<h1>Perkenalkan</h1>";
+        echo"Nama saya Wahyu Darma Nuraprilian Saya tinggal di daerah tomang yang saya sukai adalah sepakbola";
+    }
+}
